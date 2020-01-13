8d3457ef78e3c84afaf0064692c67927df278c0a
Vladimir Okun <vladimir@okun.com>
2020-01-13 18:46:36 +0200

    added color pallette defaults

--- src/config/styles.js
+++ src/config/styles.js
@@ -17,3 +17,31 @@ export const primaryColors = css`
   background: #5cdb95;
   color: #05385b;
 `;
+
+const bgColor = "#363333";
+const secondary = "#272121";
+const secondaryText = "#D8DEDE";
+const primary = "#2db14b";
+const bgText = "#f6e9e9";
+const positive = "#008c5a";
+const negative = "#ca0303";
+const boxShadowColor = "rgba(0, 0, 0, 0.59)";
+
+const boxShadowDimensions = "4px 4px 5px 0px";
+
+const palette = {
+  body: bgColor,
+  text: bgText,
+  primary,
+  primaryText: bgText,
+  secondary,
+  secondaryText: secondaryText,
+  inverse: bgText,
+  inverseText: bgColor,
+  positive,
+  negative,
+  dark: bgColor,
+  boxShadow: boxShadowColor
+};
+
+export default palette;
